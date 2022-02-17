059b5ea0143d4ccae8be161e4bb1f68969c0f6fd
LilyB095 <lily.barton095@gmail.com>
2022-02-16 20:49:20 -0700

    Add project files.

---
+++ Bookstore/Startup.cs
@@ -0,0 +1,60 @@
+using Bookstore.Models;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Bookstore
+{
+    public class Startup
+    {
+        // This method gets called by the runtime. Use this method to add services to the container.
+        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+
+        //SET UP CONFIGURATION
+        public IConfiguration Configuration { get; set; }
+        public Startup(IConfiguration temp)
+        {
+            Configuration = temp;
+        }
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+            // WE'RE USING THE MVC MODEL :D
+            services.AddControllersWithViews();
+            // SET UP CONNECTION TO DATABASE
+            services.AddDbContext<BookstoreContext>(options =>
+            {
+                options.UseSqlite(Configuration["ConnectionStrings:BookDBConnection"]);
+            });
+            services.AddScoped<IBookstoreRepository, EFBookstoreRepository>();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            // WE WANT ACCESS TO FILES IN WWWROOT FOLDER
+            app.UseStaticFiles();
+
+            app.UseRouting();
+
+            app.UseEndpoints(endpoints =>
+            {
+                // SIMPLE WAY OF SETTING UP DEFAULT ROUTE
+                endpoints.MapDefaultControllerRoute();
+            });
+        }
+    }
+}
